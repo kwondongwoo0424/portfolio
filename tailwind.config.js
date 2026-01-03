7847469d8ed8da63be498930d5347ceb6a372081
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2026-01-03 22:51:03 +0900

    update format

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,18 +9,9 @@ module.exports = {
     extend: {
       keyframes: {
         breathe: {
-          "0%": {
-            transform: "scale(1)",
-            opacity: "0.5",
-          },
-          "50%": {
-            transform: "scale(2)",
-            opacity: "0.2",
-          },
-          "100%": {
-            transform: "scale(2.2)",
-            opacity: "0",
-          },
+          "0%": { transform: "scale(1)", y: "0.5" },
+          "50%": { transform: "scale(2)", opacity: "0.2" },
+          "100%": { transform: "scale(2.2)", opacity: "0" },
         },
       },
       animation: {
